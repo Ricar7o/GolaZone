102a468846b6a8f0ac3889cc08ce18e866c260fd
Ricardo Cervantes <ricardo07@gmail.com>
2013-07-13 16:37:42 -0500

    Another initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GolaZone::Application.config.secret_token = '99786752d04475c62f5d560b3f050bc53f24ade15520e3b9c1e6d67a2c0404bb971d6f493be4b80bd967d8c55022bfc3941c3658396db9839240e794eb87bb3c'
