c0008c93d54d19052af5c763b9a72a8e8dd6d755
Ricardo Cervantes <ricardo07@gmail.com>
2013-08-05 11:47:58 -0500

    Move the update methods that are called after every match is saved to the Week model. Create specs for update_reschedules in the Week model

--- app/models/week.rb
+++ app/models/week.rb
@@ -6,4 +6,30 @@ class Week < ActiveRecord::Base
 
   validates :tournament_id, :week_number, presence: true
   validates :tournament_id, uniqueness: { scope: :week_number }
+
+  # After every match is created, verify if this is the latest match of the week.
+  # If it is, update the weeks table with this match's time.
+  # Only do this if this is a regularly scheduled match and hasn't been rescheduled.
+  def update_latest_match(match)
+    if !match.rescheduled && match.match_time > self.latest_match
+      self.latest_match = match.match_time
+      # week.save
+    end
+  end
+
+  # After every match is created, verify if this is the earliest match of the week.
+  # If it is, update the weeks table with this match's time.
+  # Only do this if this is a regularly scheduled match and hasn't been rescheduled.
+  def update_earliest_match(match)
+    if !match.rescheduled && match.match_time < self.earliest_match
+      self.earliest_match = match.match_time
+      # week.save
+    end
+  end
+
+  def update_reschedules(match)
+    self.rescheduled_matches = self.rescheduled_matches || match.rescheduled
+    # week.save
+  end
+
 end
