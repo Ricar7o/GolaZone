07da6d829a60d78bf801b7d6c048a948931fd4a4
Ricardo Cervantes <ricardo07@gmail.com>
2013-08-07 11:05:03 -0500

    Display selected results when user navigates to a campaign.

--- app/helpers/picks_helper.rb
+++ app/helpers/picks_helper.rb
@@ -1,2 +1,6 @@
 module PicksHelper
+
+  def picked_result?(match_id, result)
+    !current_user.competitions.where(campaign_id: @campaign.id).first.picks.where(match_id: match_id, selected_result: result).blank?
+  end
 end
