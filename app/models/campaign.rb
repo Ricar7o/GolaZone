62ce5921e4f660a97c00225efa1fa17c787cf35c
Ricardo Cervantes <ricardo07@gmail.com>
2013-08-13 19:43:22 -0500

    Create blank methods to generate the leaderboard

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -10,4 +10,26 @@ class Campaign < ActiveRecord::Base
 
   validates :name, :number_of_matchdays, :win_points, :lose_points, :contest_type, presence: true
 
+  # Generates a leaderboard for the campaign up to week_number
+  #
+  # week_number - the Integer number of the week
+  #
+  # Returns the leaderboard
+  def generate_week_leaderboard(week_number)
+    self.competitions.each do |comp|
+      # comp.picks - all picks by a user in a campaign
+      # w = Week.where(week_number: week_number).first
+      # w.latest_match
+      # get picks from matches that belong to a week before this one, and the match has already been played.
+
+    end
+  end
+
+  # Generates a leaderboard for the campaign up to now
+  #
+  # Returns the live leaderboard
+  def generate_live_leaderboard
+
+  end
+
 end
